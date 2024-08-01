00d8f57316385c3cb432c4fe4834d118f5bd9ddc
Shivam kumar <129588211+Shivam-kumar-3198@users.noreply.github.com>
2024-08-01 02:29:17 +0000

    /workspaces/Open_CV/readvideo.py

--- readvideo.py
+++ readvideo.py
@@ -7,7 +7,7 @@ while True:
     isTrue, frame = capture.read()
     cv.imshow('Video', frame )
 
-    if cv.waitKey(20) & 0xFF==ord('d'):
+    if cv.waitKey(50) & 0xFF==ord('d'):
         break
 
 capture.release()
